4c8646f7c538375ec94303fde1954c6198e77da2
ROY <you@example.com>
2022-09-04 20:19:33 +0600

    added-catadetails

--- js/app.js
+++ js/app.js
@@ -7,15 +7,52 @@ const loadData =() =>{
 
 const displayCatagory = catagories => {
     const cataContainer= document.getElementById('navbarNav');
+    
     catagories.forEach(category => {
         const cataUl = document.createElement('ul');
         cataUl.classList.add('navbar-nav','mx-auto')
         cataUl.innerHTML = `
         <li class="nav-item">
-        <a class="nav-link hover2" aria-current="page" href="#">${category.category_name}</a>
+        <a onclick="loadCategoryDetails('${category.category_id}')" class="nav-link hover2" aria-current="page" href="#">${category.category_name}</a>
       </li>
         `
         cataContainer.appendChild(cataUl);
     })
+}
+
+const loadCategoryDetails= id => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategoryDetails(data.data))
+}
+
+const displayCategoryDetails = ditails => {
+    const detailContainer = document.getElementById('card-container');
+    detailContainer.innerHTML='';
+    ditails.forEach(detail => {
+        const detailDiv = document.createElement('div');
+        detailDiv.classList.add('card','mb-3')
+        detailDiv.innerHTML=`
+        <div class="row">
+                 <div class="col-md-4 ">
+                    <img src="${detail.image_url}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <a class="cata-title" onclick="loadDetails()" class="card-title">${detail.title}</a>
+                        <p class="card-text text-truncate ">${detail.details}</p>
+                        <div class="row mt-5">
+                        <P class="col-6"> <i class="fa-solid fa-user"></i><span class="ms-2">${detail.author.name}</span> </P>
+                        <p class="card-text col-6"><small class="text-muted"><i class="fa-solid fa-eye"></i> ${detail.total_view}</small></p>
+                        </div>
+                        
+                    </div>
+                </div>
+</div>
+        `
+        detailContainer.appendChild(detailDiv);
+    })
+
 }
 loadData();
