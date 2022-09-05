f76174117a7294330f4aef2e7901ad6df8fb71bc
ROY <you@example.com>
2022-09-05 12:48:02 +0600

    no-news-added

--- js/app.js
+++ js/app.js
@@ -36,6 +36,20 @@ const loadCategoryDetails= id => {
 const displayCategoryDetails = ditails => {
     const detailContainer = document.getElementById('card-container');
     detailContainer.innerHTML='';
+
+    // Spinner start
+    const loadSpin = document.getElementById('spinner')
+    
+
+    // Display No News Start
+    const noNews = document.getElementById('no-news')
+    if(ditails.length === 0){
+     noNews.classList.remove('d-none')
+    }else{
+     noNews.classList.add('d-none')
+    }
+    // Display No news end
+
     ditails.forEach(detail => {
         const detailDiv = document.createElement('div');
         detailDiv.classList.add('card','mb-3')
@@ -49,7 +63,7 @@ const displayCategoryDetails = ditails => {
                         <a onclick="loadNewsDetails('${detail._id}')" class="cata-title card-title" data-bs-toggle="modal" data-bs-target="#news-detail-modal">${detail.title}</a>
                         <p class="card-text text-truncate ">${detail.details}</p>
                         <div class="row mt-5">
-                        <P class="col-6"> <i class="fa-solid fa-user"></i><span class="ms-2">${detail.author.name}</span> </P>
+                        <P class="col-6"> <i class="fa-solid fa-user"></i><span class="ms-2">${detail.author.name}</span><span id="no-data" class="d-none text-danger">No Data Found</span> </P>
                         <p class="card-text col-6"><small class="text-muted"><i class="fa-solid fa-eye"></i> ${detail.total_view}</small></p>
                         </div>
                         
@@ -74,7 +88,7 @@ const displayNewsDetails = news => {
           modalLable.innerText=`${news.title}`
     const modalContainer = document.getElementById('modal-container')
     modalContainer.innerHTML=`
-    <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+    <img src="${news.image_url}" class="img-fluid rounded-start mb-2" alt="...">
                         <p>${news.details}</p>
                         <div class="row mt-5">
                         <P class="col-6"> <i class="fa-solid fa-user"></i><span class="ms-2">${news.author.name}</span> </P>
